a3990a1fc36c80c4c57594aba9a052e494a4dde4
York <York.Yanmeng@gmail.com>
2023-12-22 18:50:10 +0800

    2023/12/22

--- src/main/java/com/york/utils/Constants.java
+++ src/main/java/com/york/utils/Constants.java
@@ -2,16 +2,22 @@ package com.york.utils;
 
 public class Constants {
 
+    // 请求成功 code 返回
     public final static int OK_CODE = 0;
 
+    // 请求失败 code 返回
     public final static int FAIL_CODE = 400;
 
+    // 请求成功 msg 返回
     public final static String OK_MSG = "请求成功";
 
+    // 请求失败 msg 返回
     public final static String FAIL_MSG = "请求失败";
 
+    // 未登录 code 返回
     public final static int STATUS_0 = 0;
 
+    // 已登录 code 返回
     public final static int STATUS_1 = 1;
 
     public final static String CACHE_NAME = "KACache";
